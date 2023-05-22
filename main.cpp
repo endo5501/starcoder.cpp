941e69edfc4a9f397cc1449874b501f75fb4f2ae
endo5501 <endo5501@users.noreply.github.com>
2023-05-23 00:06:40 +0900

    windows

--- main.cpp
+++ main.cpp
@@ -12,7 +12,25 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#include <io.h>
+
+#define STDIN_FILENO _fileno( stdin )
+#define STDOUT_FILENO _fileno( stdout )
+#define STDERR_FILENO _fileno( stdout )
+
+#define isatty _isatty
+
+#undef min
+#undef max
+
+#else
 #include <unistd.h>
+#endif
 
 // default hparams (GPT-2 117M)
 // https://huggingface.co/bigcode/gpt_bigcode-santacoder/blob/main/config.json
